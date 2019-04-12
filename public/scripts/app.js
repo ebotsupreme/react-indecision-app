2f6dfd98d7e6f1ee4dbc354a0cd9c1ce277af2b9
Eddie Jung <ejung716@gmail.com>
2019-04-12 13:40:47 -0700

    added createElement call

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,10 @@
 console.log('App.js is running!');
 
 // JSX - JavaScript XML
-var template = <p>This is JSX from app.js!</p>;
+// var template = <p>This is JSX from app.js!</p>;
+var template = React.createElement("p", {
+    id: "someId"
+  }, "This is JSX from app.js!");
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
