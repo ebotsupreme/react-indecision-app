d39d3f36d5805adbb3682aed35c6fde823bd031d
Eddie Jung <ejung716@gmail.com>
2019-04-12 15:01:08 -0700

    added additional elements

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,19 +4,32 @@ console.log('App.js is running!');
 
 // JSX - JavaScript XML
 var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
+    'div',
     null,
-    'Indecision App'
-  ),
-  ' ',
-  React.createElement(
-    'p',
-    null,
-    'This is some info'
-  )
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some info'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
 );
 var appRoot = document.getElementById('app');
 
