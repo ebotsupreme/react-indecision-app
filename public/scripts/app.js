89f97323d2c54988f5b60749bf806bfa3affdb70
Eddie Jung <ejung716@gmail.com>
2019-04-16 13:32:09 -0700

    es6 arrow function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,90 +1,11 @@
-'use strict';
+"use strict";
 
-console.log('App.js is running!');
-
-var app = {
-    title: 'Indecision App',
-    subtitle: 'Some extra info',
-    options: ['One', 'Two']
-
-    // JSX - JavaScript XML
-};var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        app.title
-    ),
-    app.subtitle && React.createElement(
-        'p',
-        null,
-        'Subtitle: ',
-        app.subtitle
-    ),
-    React.createElement(
-        'p',
-        null,
-        app.options.length > 0 ? 'Here are your options' : 'No options'
-    ),
-    React.createElement(
-        'ol',
-        null,
-        React.createElement(
-            'li',
-            null,
-            'Item one'
-        ),
-        React.createElement(
-            'li',
-            null,
-            'Item two'
-        )
-    )
-);
-
-// let userName = 'Mikey Jones';
-// let userAge = 27;
-// let userLocation = 'Los Angeles';
-
-var user = {
-    name: 'Eddie',
-    age: 26,
-    location: 'Philly'
+var square = function square(x) {
+    return x * x;
 };
 
-function getLocation(location) {
+var squareArrow = function squareArrow(x) {
+    return x * x;
+};
 
-    if (location) {
-        return React.createElement(
-            'p',
-            null,
-            'Location: ',
-            location
-        );
-    }
-}
-var templateTwo = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        user.name ? user.name : 'Anonymous'
-    ),
-    user.age && user.age >= 18 && React.createElement(
-        'p',
-        null,
-        'Age: ',
-        user.age
-    ),
-    getLocation(user.location)
-);
-
-var appRoot = document.getElementById('app');
-
-var appRoot2 = document.getElementById('app2');
-
-ReactDOM.render(template, appRoot);
-
-ReactDOM.render(templateTwo, appRoot2);
+console.log(squareArrow(9));
