00fd7fe1bcd5055d5c7b5a9aa88633b12dfc56b9
Eddie Jung <ejung716@gmail.com>
2019-04-16 13:53:54 -0700

    two examples getting first and last name

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,31 @@
-"use strict";
+'use strict';
 
-var square = function square(x) {
+function square(x) {
     return x * x;
 };
+console.log(square(3));
 
+// regular arrow func
+// const squareArrow = (x) => 
+// {
+//     return x * x;
+// };
+
+// express arrow func
 var squareArrow = function squareArrow(x) {
     return x * x;
 };
-
 console.log(squareArrow(9));
+
+var firstName = void 0;
+var getFirstName = function getFirstName(fullName) {
+    firstName = fullName.split(' ')[0];
+    return firstName;
+};
+console.log(getFirstName('Eezy DoesIt'));
+
+var lastName = void 0;
+var getLastName = function getLastName(fullName) {
+    return lastName = fullName.split(' ')[1];
+};
+console.log(getLastName('Lem Peezy'));
--- src/playground/es6-arrow-function.js
+++ src/playground/es6-arrow-function.js
@@ -1,10 +1,26 @@
-const square = function (x) 
+function square (x) 
 {
     return x * x;
 };
+console.log(square(3));
 
-const squareArrow = (x) => {
-    return x * x;
-};
+// regular arrow func
+// const squareArrow = (x) => 
+// {
+//     return x * x;
+// };
 
+// express arrow func
+const squareArrow = (x) => x * x;
 console.log(squareArrow(9));
+
+let firstName;
+const getFirstName = (fullName) => {
+    firstName = fullName.split(' ')[0];
+    return firstName;
+}
+console.log(getFirstName('Eezy DoesIt'));
+
+let lastName;
+const getLastName = (fullName) => lastName = fullName.split(' ')[1];
+console.log(getLastName('Lem Peezy'));
