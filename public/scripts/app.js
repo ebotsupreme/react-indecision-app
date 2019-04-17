8a5501362220809de3859f0e1de19d875586be7b
Eddie Jung <ejung716@gmail.com>
2019-04-17 13:34:22 -0700

    added render funciton to increase count for the length of option array.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,58 +5,77 @@ console.log('App.js is running!');
 var app = {
     title: 'Indecision App',
     subtitle: 'Some extra info',
-    options: ['One', 'Two']
+    options: []
 };
 
 var onFormSubmit = function onFormSubmit(e) {
     e.preventDefault(); // stop page refresh
-    console.log('form submitted!');
+
+    var option = e.target.elements.option.value; // target is synthetic event, elemtns are what is avail to us in the dom, option is the name of input
+
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+    }
+
+    // re-render
+    renderFunction();
 };
 
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        app.title
-    ),
-    app.subtitle && React.createElement(
-        'p',
-        null,
-        'Subtitle: ',
-        app.subtitle
-    ),
-    React.createElement(
-        'p',
-        null,
-        app.options.length > 0 ? 'Here are your options' : 'No options'
-    ),
-    React.createElement(
-        'ol',
+var renderFunction = function renderFunction() {
+    var template = React.createElement(
+        'div',
         null,
         React.createElement(
-            'li',
+            'h1',
             null,
-            'Item one'
+            app.title
+        ),
+        app.subtitle && React.createElement(
+            'p',
+            null,
+            'Subtitle: ',
+            app.subtitle
         ),
         React.createElement(
-            'li',
+            'p',
             null,
-            'Item two'
-        )
-    ),
-    React.createElement(
-        'form',
-        { onSubmit: onFormSubmit },
-        React.createElement('input', { type: 'text', name: 'option' }),
+            app.options.length > 0 ? 'Here are your options' : 'No options'
+        ),
         React.createElement(
-            'button',
+            'p',
             null,
-            'Add Option'
+            app.options.length
+        ),
+        React.createElement(
+            'ol',
+            null,
+            React.createElement(
+                'li',
+                null,
+                'Item one'
+            ),
+            React.createElement(
+                'li',
+                null,
+                'Item two'
+            )
+        ),
+        React.createElement(
+            'form',
+            { onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'option' }),
+            React.createElement(
+                'button',
+                null,
+                'Add Option'
+            )
         )
-    )
-);
+    );
+    ReactDOM.render(template, appRoot);
+};
 
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+
+// initialize app render
+renderFunction();
--- src/app.js
+++ src/app.js
@@ -3,30 +3,48 @@ console.log('App.js is running!');
 const app = {
     title: 'Indecision App',
     subtitle: 'Some extra info',
-    options: ['One', 'Two']
+    options: []
 }
 
 const onFormSubmit = (e) => {
     e.preventDefault(); // stop page refresh
-    console.log('form submitted!');
+
+    const option = e.target.elements.option.value; // target is synthetic event, elemtns are what is avail to us in the dom, option is the name of input
+
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+    }
+
+    // re-render
+    renderFunction();
+}
+
+
+const renderFunction = () => {
+    const template = (
+        <div>
+            <h1>{app.title}</h1> 
+            {app.subtitle && <p>Subtitle: {app.subtitle}</p>}
+            <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+            <p>{app.options.length}</p>
+            <ol>
+                <li>Item one</li>
+                <li>Item two</li>
+            </ol>
+            <form onSubmit={onFormSubmit}> 
+                <input type="text" name="option"/>
+                <button>Add Option</button>
+            </form>
+        </div>
+    );
+    ReactDOM.render(template, appRoot);
 
 }
 
-const template = (
-    <div>
-        <h1>{app.title}</h1> 
-        {app.subtitle && <p>Subtitle: {app.subtitle}</p>}
-        <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-        <ol>
-            <li>Item one</li>
-            <li>Item two</li>
-        </ol>
-        <form onSubmit={onFormSubmit}> 
-            <input type="text" name="option"/>
-            <button>Add Option</button>
-        </form>
-    </div>
-);
+
 
 const appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+
+// initialize app render
+renderFunction();
