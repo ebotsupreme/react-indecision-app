2c8d9abe92f5ad806fb787f21e25700f85511d04
Eddie Jung <ejung716@gmail.com>
2019-04-17 13:01:03 -0700

    counter now working. Put templateTwo and reactDOM.render inside renderCounterApp function.

--- src/app.js
+++ src/app.js
@@ -22,7 +22,8 @@ const template = (
 
 let count = 0;
 const addOne = () => {
-    console.log('addOne');
+    count++;
+    renderCounterApp();
 };
 const minusOne = () => {
     console.log('minusOne');
@@ -32,18 +33,22 @@ const reset = () => {
 };
 
 
-const templateTwo = (
-    <div>
-        <h1>Count: {count}</h1>
-        <button onClick={addOne} >+1</button>
-        <button onClick={minusOne}>-1</button>
-
-        <button onClick={reset}>Reset</button>
-
-    </div>
-);
-
-
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+const renderCounterApp = () => {
+    const templateTwo = (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={addOne} >+1</button>
+            <button onClick={minusOne}>-1</button>
+    
+            <button onClick={reset}>Reset</button>
+    
+        </div>
+    );
+
+    ReactDOM.render(templateTwo, appRoot);
+
+}
+
+renderCounterApp();
