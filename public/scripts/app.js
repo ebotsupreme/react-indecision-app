147a6c2ab0ecd6881e3f1aa0a74a32c03aa6cbf4
Eddie Jung <ejung716@gmail.com>
2019-04-18 15:29:50 -0700

    classes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,55 +1,9 @@
 "use strict";
 
-// Visibility Toggle App
-var myMsg = "Hey. Theres are some details you can now";
-var initialDetail = "Show Details";
-var showDetails = "Show Details";
-var hideDetails = "Hide Details";
-var toggleMsg = void 0;
-var temp = void 0;
-var counter = 0;
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-// function
-var toggle = function toggle() {
-    toggleMsg = myMsg;
-    var toggleDetails = document.getElementById('detailBtn').innerHTML;
-    if (toggleDetails == showDetails) {
-        counter = 1;
-        toggleDetails = document.getElementById('detailBtn').innerHTML = hideDetails;
-    } else if (toggleDetails == hideDetails) {
-        counter = 0;
-        toggleDetails = document.getElementById('detailBtn').innerHTML = showDetails;
-        toggleMsg = '';
-    }
-    render();
+var Person = function Person() {
+  _classCallCheck(this, Person);
 };
 
-// element to show
-var appRoot = document.getElementById('app');
-
-var render = function render() {
-    // template
-    var template = React.createElement(
-        "div",
-        null,
-        React.createElement(
-            "h1",
-            null,
-            "Visibility Toggle"
-        ),
-        React.createElement(
-            "button",
-            { id: "detailBtn", onClick: toggle },
-            counter == 0 ? showDetails : hideDetails
-        ),
-        React.createElement(
-            "p",
-            null,
-            toggleMsg
-        )
-    );
-    // reactdom
-    ReactDOM.render(template, appRoot);
-};
-// render func call
-render();
+var me = new Person();
