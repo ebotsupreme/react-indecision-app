0a3b0d55a69f0d027029c455847d25e26ab2ed92
Eddie Jung <ejung716@gmail.com>
2019-04-18 15:38:08 -0700

    added this.name and getGreeting().

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -1,6 +1,17 @@
 
 class Person {
+    constructor(name = 'John Snow') {
+        this.name = name;
+    }
+
+    getGreeting() {
+        return 'Hi!';
+    }
 
 }
 
-const me = new Person();
+const me = new Person('Eddie Jung');
+console.log(me.getGreeting());
+
+const other = new Person();
+console.log(other.getGreeting());
