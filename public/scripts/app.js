517b8019be6009d1454fc54f1d2385c7c8cb06ce
Eddie Jung <ejung716@gmail.com>
2019-04-18 14:19:43 -0700

    added check to toggle show detail btn to hide on click

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,16 +1,32 @@
-'use strict';
+"use strict";
 
 // Visibility Toggle
 
 // array
 var myMsg = "Hey. Theres are some details you can now";
+var showDetails = "Show Details";
+var hideDetails = "Hide Details";
 var toggleMsg = void 0;
+var temp = void 0;
 
 // function
 var toggle = function toggle() {
     console.log('toggle on');
     toggleMsg = myMsg;
     console.log(toggleMsg);
+    var toggleDetails = document.getElementById('detailBtn').innerHTML;
+    console.log('ssss');
+    console.log(toggleDetails);
+    console.log(showDetails);
+    console.log(hideDetails);
+    if (toggleDetails == showDetails) {
+        showDetails = hideDetails;
+        console.log('aaa', showDetails);
+    } else if (toggleDetails == hideDetails) {
+        temp = showDetails;
+        showDetails = temp;
+        console.log('bbb', showDetails);
+    }
     render();
 };
 
@@ -20,20 +36,20 @@ var appRoot = document.getElementById('app');
 var render = function render() {
     // template
     var template = React.createElement(
-        'div',
+        "div",
         null,
         React.createElement(
-            'h1',
+            "h1",
             null,
-            'Visibility Toggle'
+            "Visibility Toggle"
         ),
         React.createElement(
-            'button',
-            { onClick: toggle },
-            'Show details'
+            "button",
+            { id: "detailBtn", onClick: toggle },
+            showDetails
         ),
         React.createElement(
-            'p',
+            "p",
             null,
             toggleMsg
         )
--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -2,13 +2,31 @@
 
 // array
 const myMsg = "Hey. Theres are some details you can now";
+let showDetails = "Show Details";
+let hideDetails = "Hide Details";
 let toggleMsg;
+let temp;
 
 // function
 const toggle = () => {
     console.log('toggle on');
     toggleMsg = myMsg;
     console.log(toggleMsg);
+    const toggleDetails = document.getElementById('detailBtn').innerHTML;
+    console.log('ssss');
+    console.log(toggleDetails);
+    console.log(showDetails);
+    console.log(hideDetails);
+    if (toggleDetails == showDetails) {
+        showDetails = hideDetails;
+        console.log('aaa', showDetails);
+
+    } else if (toggleDetails == hideDetails) {
+        temp = showDetails;
+        showDetails = temp;
+        console.log('bbb', showDetails);
+
+    }
     render();
 }
 
@@ -20,9 +38,8 @@ const render = () => {
     const template = (
         <div>
             <h1>Visibility Toggle</h1>
-            <button onClick={toggle}>Show details</button>
-            
-                <p>{toggleMsg}</p>
+            <button id="detailBtn" onClick={toggle}>{showDetails}</button>
+            <p>{toggleMsg}</p>
             
         </div>
     );
