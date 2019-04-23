64c0ff2a81412db4312b5e56bf6dc7d04899f2f7
Eddie Jung <ejung716@gmail.com>
2019-04-23 14:28:07 -0700

    restored visibility toggle app file to original.

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,42 +1,27 @@
-// Visibility Toggle App
-const myMsg = "Hey. Theres are some details you can now";
-let initialDetail = "Show Details";
-let showDetails = "Show Details";
-let hideDetails = "Hide Details";
-let toggleMsg;
-let temp;
-let counter = 0;
+// visibilityToggle App
+
+let visibility = false;
 
 // function
-const toggle = () => {
-    toggleMsg = myMsg;
-    let toggleDetails = document.getElementById('detailBtn').innerHTML;
-    if (toggleDetails == showDetails) {
-        counter = 1;
-        toggleDetails = document.getElementById('detailBtn').innerHTML = hideDetails;        
-    } else if (toggleDetails == hideDetails) {
-        counter = 0;
-        toggleDetails = document.getElementById('detailBtn').innerHTML = showDetails;
-        toggleMsg = '';
-    }
+const toggleVisibility = () => {
+    visibility = !visibility;
     render();
 }
 
-// element to show
-const appRoot = document.getElementById('app');
-
 const render = () => {
-    // template
-    const template = (
+    const jsx = (
         <div>
             <h1>Visibility Toggle</h1>
-            <button id="detailBtn" onClick={toggle}>{(counter == 0) ? showDetails : hideDetails}</button>
-            <p>{toggleMsg}</p>
-            
+            <button id="detailBtn" onClick={toggleVisibility}>{visibility ? 'Hide details' : 'Show details'}</button>
+            {visibility && (
+                <div>
+                    <p>Hey. These are some details you can now see!</p>
+                </div>    
+            )}            
         </div>
     );
     // reactdom
-    ReactDOM.render(template, appRoot);
+    ReactDOM.render(jsx, document.getElementById('app'));
 }
 // render func call
 render();
