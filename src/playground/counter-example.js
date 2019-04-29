5d297273fb05b731e46a8c7ae04a0ef705529a7d
Eddie Jung <ejung716@gmail.com>
2019-04-29 15:44:35 -0700

    added babel and yarn scripts to package.json to avoid global modules going forward. Now using local modules for babel and live-server.

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -17,7 +17,7 @@ class Counter extends React.Component {
             const stringCount = localStorage.getItem('count');
             const count = parseInt(stringCount, 10);
 
-            if (count) {
+            if (!isNaN(count)) {
                 this.setState(() => ({ count }));
             }
 
